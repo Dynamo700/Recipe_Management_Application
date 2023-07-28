922622bf256fabe6cdff64d9e18fa2544eb26c3d
Nick <nickq4504@gmail.com>
2023-07-28 18:10:17 -0230

    The page for adding recipes, the page of removing recipes and the page for viewing recipes have been created, and have been connected using navigation.

--- project/application.py
+++ project/application.py
@@ -3,13 +3,25 @@ import pandas as pd
 
 app = Flask(__name__)
 
+#Home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/variabletest/<name>')
-# def print_variable(name):
-#     return 'Hello %s' % name
+#Recipe page
+@app.route('/Recipe_page')
+def Recipe_page():
+    return render_template("Recipe_page.html")
+
+#Page for adding recipes
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("add_recipe.html")
+
+#Page for removing recipes
+@app.route('/remove_recipe')
+def remove_recipe():
+    return render_template("remove_recipe.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
